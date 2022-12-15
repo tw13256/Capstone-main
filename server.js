c7186458d3b3dc7846b2a081486eaf723dc57db4
James Huang <james@mcsh.co>
2022-12-15 03:46:57 +0000

    Fixed const server problem.

--- server.js
+++ server.js
@@ -1,69 +1,71 @@
-const express = require('express')
-const app = express()
-const server = require('https').Server(app)
-const fs=require('fs')
-const io = require('socket.io')(server)
-const peer = require('peer');
+const express = require("express");
+const app = express();
+const fs = require("fs");
+const options = {
+  key: fs.readFileSync("key.pem"),
+  cert: fs.readFileSync("cert.pem"),
+  //可以考慮直接用node-forge來生
+};
+const server = require("https").Server(options, app);
+const io = require("socket.io")(server);
+const peer = require("peer");
 //const { v4: uuidV4 } = require('uuid')
 
-var options={ key: fs.readFileSync('/home/B10902028/server-key.pem'),
-ca: [fs.readFileSync('/home/B10902028/cert.pem')],
-cert: fs.readFileSync('/home/B10902028/server-cert.pem')}
-
-console.log('start server') 
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-app.use(express.static('views'))
-
+console.log("start server");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.static("views"));
 
 // app.get('/', (req, res) => {
 //   res.redirect(`/${uuidV4()}`)
 // })
-const users = {}
+const users = {};
 // 測試
-const i=0;
-app.get('/:room', (req, res) => {
-  res.render('room', { roomId: req.params.room })
+const i = 0;
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.params.room });
   //console.log("room")
-})
+});
 
-let peerIds = {} // Key: socket.id, Value: peerId
-io.on('connection', socket => {
-  socket.on('join-room', (roomId, userId) => {
+let peerIds = {}; // Key: socket.id, Value: peerId
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId) => {
+    socket.join(roomId);
+    socket.to(roomId).emit("user-connected", userId);
+    peerIds[socket.id] = userId;
+    //console.log(roomId,userId,'joined-room')
 
-    socket.join(roomId)
-    socket.to(roomId).emit('user-connected', userId)
-    peerIds[socket.id] = userId
-    //console.log(roomId,userId,'joined-room') 
-
-    socket.on('new-user', name => {
-      users[socket.id] = name
-      io.emit('user-connected__', name)
+    socket.on("new-user", (name) => {
+      users[socket.id] = name;
+      io.emit("user-connected__", name);
       // console.log
       //console.log(users[socket.Id])
-      console.log(name,'new user connected')
-    })
+      console.log(name, "new user connected");
+    });
 
-    socket.on('send-chat-message', message => {
-      io.emit('chat-message', { name: users[socket.id], message: message })
+    socket.on("send-chat-message", (message) => {
+      io.emit("chat-message", { name: users[socket.id], message: message });
       // console.log chat message
-      console.log(users[socket.id],':chat message sent',message) 
-    })
+      console.log(users[socket.id], ":chat message sent", message);
+    });
 
-    socket.on('disconnect', () => {
+    socket.on("disconnect", () => {
       //socket.to(roomId).emit('user-disconnected', userId)
-      io.emit('user-disconnected', users[socket.id])
+      io.emit("user-disconnected", users[socket.id]);
       // disconnect 可以觸發
-      io.emit('remove-video-' + peerIds[socket.id])
-      console.log(users[socket.id], 'user disconnected')
-      delete users[socket.id]
-      delete peerIds[socket.id]
-    })
-  }) 
-})
+      io.emit("remove-video-" + peerIds[socket.id]);
+      console.log(users[socket.id], "user disconnected");
+      delete users[socket.id];
+      delete peerIds[socket.id];
+    });
+  });
+});
 
-let myPeerServer = peer.PeerServer({ 
-  port: 3001, 
-  path: '/'
-})
-server.listen(443)
+let myPeerServer = peer.PeerServer({
+  port: 3001,
+  path: "/",
+});
+
+server.listen(443, () => {
+  console.log("Server listening on port 443");
+});
